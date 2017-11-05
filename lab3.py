d41092786d99b3a012abfa4847445ceb7c33cc27
noamm19-meet <noamm19@meet.mit.edu>
2017-11-05 18:03:40 +0200

    bla

--- lab3.py
+++ lab3.py
@@ -1,4 +1,16 @@
 import turtle
 turtle.addshape("Feather-Transparent.gif")
-turtle.shape("Feather-Transparent.gif") 
+turtle.shape("Feather-Transparent.gif")
+angle=7
+turtle.speed(3000)
+for i in range(121):
+	turtle.forward(300)
+	turtle.right(60)
+	turtle.forward(100)
+	turtle.right(90)
+	turtle.forward(70)
+	turtle.home()
+	turtle.right(angle)
+	angle=angle+3
+	 
 turtle.mainloop()
